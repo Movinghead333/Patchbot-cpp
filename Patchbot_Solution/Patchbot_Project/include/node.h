22b7ff789fd96bd5697319096e02f49edf20719f
Movinghead333 <Movinghead333@googlemail.com>
2019-01-17 08:30:04 +0100

    vc: 92

--- Patchbot_Solution/Patchbot_Project/include/node.h
+++ Patchbot_Solution/Patchbot_Project/include/node.h
@@ -1,14 +1,21 @@
 #ifndef __NODE_H_DEFINED__
 #define __NODE_H_DEFINED__
 
+// data struct Node used for calculating the dijkstra algorithm for pathfinding
 struct Node
 {
+	// x coordinate of the represented tile
 	int m_x;
+	// x coordinate of the represented tile
 	int m_y;
+
+	// distance needed to calculate the dijkstra algorithm
 	int m_distance;
 
+	// parameterized constructor
 	Node(int p_x, int p_y, int p_distance);
 
+	// less operator needed for use in std::priority_queue
 	bool operator<(const Node& other_node) const;
 };
 
--- Patchbot_Solution/Patchbot_Project/src/node.cpp
+++ Patchbot_Solution/Patchbot_Project/src/node.cpp
@@ -1,5 +1,7 @@
 #include "node.h"
 
+// constructor which takes in the coordinates of a tile and their distance to
+// the starting Node
 Node::Node(
 	int p_x,
 	int p_y,
@@ -11,6 +13,8 @@ Node::Node(
 {
 }
 
+// compares two Node object based on their distance to the original starting
+// Node
 bool Node::operator<(const Node & other_node) const
 {
 	return other_node.m_distance < m_distance;
